fee5b3930d0bbcc16f6e4ee65bac91db3ef198bc
christopherfickess <cfickess20@gmail.com>
2026-02-18 10:24:25 -0600

    Updating things for Gateway and LB

--- stacks/azure/mattermost-postgres/key-vault.tf
+++ stacks/azure/mattermost-postgres/key-vault.tf
@@ -182,12 +182,8 @@ resource "azurerm_key_vault_secret" "database_connection_string" {
 
   name         = var.db_connection_string_name
   # value        = var.db_internal_username
-  value = "postgresql://${var.db_internal_username}:${random_password.postgres_internal_password.result}@${module.mattermost_postgres.host}:${module.mattermost_postgres.port}/${module.mattermost_postgres.dbname}?sslmode=require"
+  value = "postgres://${var.db_internal_username}:${random_password.postgres_internal_password.result}@${module.mattermost_postgres.host}:${module.mattermost_postgres.port}/${module.mattermost_postgres.dbname}?sslmode=require"
   key_vault_id = azurerm_key_vault.mattermost_key_vault.id
-
-  lifecycle {
-    ignore_changes = [value]
-  }
 
   tags = merge(local.tags, { Project = "Mattermost Postgres Internal User" })
 }
